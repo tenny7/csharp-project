f617f7e38c8aff6ed0958c525b7653d87971c9f5
Ovie Tennyson <atomictenny@gmail.com>
2021-06-06 11:29:31 -0500

    First Csharp commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace csharpconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName,lastName;
+            Console.WriteLine("Enter your Firstname!");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter Lastname");
+            lastName = Console.ReadLine();
+            Console.Write("your Fullname is  {0} {1}", firstName,lastName);
+
+        }
+    }
+}
